435bf1fb2cef386596a3a203b53c5c62174d71c6
João Pedro <jpmeira020@gmail.com>
2022-05-06 14:41:52 -0300

    Commit 1

---
+++ src/main/java/org/generation/farmacia/model/Produto.java
@@ -0,0 +1,78 @@
+package org.generation.farmacia.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name= "tb_produto")
+public class Produto {
+	
+	@Id  
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id; 
+	
+	@NotNull(message = "Nome do remédio é obrigatório")
+	private String nome; 
+	
+	@NotNull(message = "Nome do laboratório é obrigatório!")
+	private String laboratorio;
+	
+	@Positive(message = "O atributo preço deve ser maior que zero!")
+	private BigDecimal preco;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLaboratorio() {
+		return laboratorio;
+	}
+
+	public void setLaboratorio(String laboratorio) {
+		this.laboratorio = laboratorio;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	} 
+	
+	
+}
